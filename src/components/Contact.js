c5ef37b66bf082b22b0d5de933531179dd975caf
yash nirmal <yash15nirmal@gmail.com>
2022-05-08 16:46:20 +0530

    almost final commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,84 @@
-import React from 'react';
 import "./Contact.css";
+import emailjs from 'emailjs-com';
+import React, { useRef } from 'react';
+// import emailjs from '@emailjs/browser';
 
 
-export default function Contact() {
+export default function Contact(props) {
+
+  const form = useRef();
+
+  function sendBtnClicked(e){
+    let mailInput = document.getElementById('mailIdInput');
+    e.preventDefault();
+
+
+    if(mailInput.value==""){
+      props.handleSnack("Mail field is Empty!","error");
+      return;
+    }
+
+    emailjs.sendForm(
+      "service_4vd67gd",
+      "template_1tfkbqk",
+      form.current,
+      "kYY-TPypqA7rKTppt"
+    )
+    .then((response,error)=>{
+      console.log(response.text);
+      props.handleSnack("Thankyou for sharing your feedback!","success");
+    })
+    .catch((err)=>console.log(err.message));
+  }
+
+
   return (
     <div className="contact-container">
       <div className="contact-div">
-        <h1>Contact Us</h1>
-        <textarea
-          className="contact-textarea"
-          placeholder="Tell us what you are thinking"
-        ></textarea>
-        <button className="send Button">Send</button>
+        <h1 style={{ marginBottom: 50 }}>Contact Us</h1>
+        
+        <form ref={form} onSubmit={sendBtnClicked}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              marginBottom: 10,
+            }}
+            >
+            <div style={{ flex: 1, marginRight: 30 }}>
+              <p>Your e-Mail</p>
+              <input
+                style={{ width: "100%", marginBottom: 5, marginTop: 5 }}
+                className="Input"
+                placeholder="Mail Id"
+                type="email"
+                id="mailIdInput"
+                name="mailId"
+              />
+            </div>
+            <div style={{ flex: 1 }}>
+              <p>Subject</p>
+              <input
+                className="Input"
+                style={{ width: "100%", marginBottom: 5, marginTop: 5 }}
+                placeholder="Subject"
+                type="text"
+                id="subjectInput"
+                name="subject"
+              />
+            </div>
+          </div>
+          <p>Description</p>
+          <textarea
+            className="Input"
+            style={{ width: "100%", height: 150, marginBottom: 20, marginTop: 5 }}
+            placeholder="Tell us what you are thinking"
+            id="descriptionInput"
+            name="description"
+          ></textarea>
+          <button className="send Button" type="submit">Send</button>
+          
+        </form>
       </div>
     </div>
   );
